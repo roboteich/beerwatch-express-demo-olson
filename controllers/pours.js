483ae47ac767be72143f3e04e74f47067f617041
Ross Teichner <rteichner@olson.com>
2015-04-11 15:37:07 -0500

    stats collecting per user per family

--- controllers/pours.js
+++ controllers/pours.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , router = express.Router()
   , Pour = require('../models/pour')
+  , Beer = require('../models/beer')
   , _ = require("underscore")
 
 
@@ -27,33 +28,79 @@ router.get("/user/:user_id/pour", function(req, res){
 
 });
 
-// router.get('/user/:user_id/stats', function(req, res){
+router.get('/user/:user_id/stats', function(req, res){
 
-//   var user = req.params.user_id;
+  var user = req.params.user_id;
 
-//   //get all the beer ids, counts of a users pours
-//   var querySpec = {
-//         query: "SELECT beer_id, poured_on FROM root r WHERE r.user_id=@user_id AND r.type='pour'",
-//         parameters:[{
-//           name:"@user_id",
-//           value:req.params.user_id
-//         }]
-//     };
+  //get all the beer ids, counts of a users pours
+  var pourQuery = {
+        query: "SELECT r.beer_id, r.poured_on FROM root r WHERE r.user_id=@user_id AND r.type='pour'",
+        parameters:[{
+          name:"@user_id",
+          value:req.params.user_id
+        }]
+    };
 
-//   Pour.find(querySpec, function(err, items){
-//     if(err){
-//         throw(err)
-//     }
+  Pour.find(pourQuery, function(err, items){
+    
+    if(err){
+        console.log(err);
+        throw(err)
+    }
       
-//     var itemsById = _.groupBy()
+    var beerCounts = {};
+    var totalPours = 0;
+    _.each(items, function(beerPour, key, list){
+      beerCount = beerPour["poured_on"].length;
+      beerCounts[beerPour["beer_id"]] = beerCount;
+      totalPours += beerCount;
+    });
 
-//   });
+    var beerIds = _.pluck(items, "beer_id");
 
-//   //get all the beer families of the 
+    //get all the beer families
+    var beerQuery = {
+       query: "SELECT r.id, r.beerFamily FROM root r"
+    }
 
-//   Pour.find(querySpec, function(err, items){
+    Beer.find(beerQuery, function(err, items){
 
-// });
+      if(err){
+        throw (err)
+      }
+
+      var pouredBeers = _.filter(items, function(item){
+        return _.contains(beerIds, item.id);
+      });
+
+      var familyCounts = {};
+
+      _.each(pouredBeers, function(beer, key, list){
+          
+        beerFamily = beer.beerFamily;
+        if(!familyCounts[beerFamily]){ familyCounts[beerFamily] = 0 }
+        familyCounts[beerFamily] += beerCounts[beer.id];
+
+      });
+
+      var families = [];
+      _.each(familyCounts, function(count, family, list){
+
+        families.push({family:family, count:count});
+
+      });
+
+      var sortedFamilies = _.sortBy(families, 'count');
+
+      res.json({families:sortedFamilies, pour_count:totalPours});
+
+    });
+
+  });
+
+  //get all the beer families of the 
+
+});
 
 router.put('/user/:user_id/pour/:beer_id', function(req, res){
 
